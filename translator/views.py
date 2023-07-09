1367538e38f6606c77d4955acef485d5f7ebead1
Eduardo Louhau <louhaudev@gmail.com>
2023-07-08 21:23:45 -0400

    Render source game after page reload.

--- translator/views.py
+++ translator/views.py
@@ -7,9 +7,9 @@ def index(request):
     translated_game = None
 
     if request.method == 'POST':
-        game = request.POST.get('text')
+        game = request.POST.get('game')
         source_language = "en"
         target_language = "es"
         translated_game = translate_pgn_game(source_language, target_language, game)
 
-    return render(request, 'translator/index.html', {'translated_game': translated_game})
+    return render(request, 'translator/index.html', {'translated_game': translated_game, 'game': game})
