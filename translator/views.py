190a7f8d94f98f196044e8ec9d4ac63f58f2ebab
Eduardo Louhau <louhaudev@gmail.com>
2023-07-10 12:27:47 -0400

    Fix scope issue whit game variable on views.py

--- translator/views.py
+++ translator/views.py
@@ -5,11 +5,12 @@ from .pgn_translator import translate_pgn_game
 
 def index(request):
     translated_game = None
-
+    game = ""
+   
     if request.method == 'POST':
         game = request.POST.get('game')
         source_language = "en"
         target_language = "es"
         translated_game = translate_pgn_game(source_language, target_language, game)
-
+    
     return render(request, 'translator/index.html', {'translated_game': translated_game, 'game': game})
