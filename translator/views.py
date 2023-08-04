a9aa0ff31232a6f631346aa4c774fa39bfbe37ad
Eduardo Louhau <louhaudev@gmail.com>
2023-08-04 09:10:36 -0400

    Refactor view.

--- translator/views.py
+++ translator/views.py
@@ -6,6 +6,10 @@ from .forms import TranslatedGame
 # Create your views here.
 
 def index(request):
+    source_lang_form = None
+    target_lang_form = None
+    context = {'source_form': source_lang_form, 'target_form': target_lang_form}
+    
     if request.method == "POST":
         source_lang_form = GameForm(request.POST)
         target_lang_form = TranslatedGame(request.POST)
@@ -16,12 +20,10 @@ def index(request):
             target_language = "es" # TODO get from game form.
             translated_game = translate_pgn_game(source_language, target_language, game)
             target_lang_form = TranslatedGame(initial={'translated_pgn': translated_game})
-            context =  {'source_form': source_lang_form, 'target_form': target_lang_form, 'translated_game': translated_game}
-    
+            context['translated_game'] = translated_game
     else:
         source_lang_form = GameForm()
         target_lang_form = TranslatedGame()
-        context = {'source_form': source_lang_form, 'target_form': target_lang_form}
     return render(request, 'translator/index.html', context)
 
 
