2bb6ed93d2a6bda810a5a8ab3e499099dd8ff7aa
Eduardo Louhau <louhaudev@gmail.com>
2023-08-06 11:22:42 -0400

    Make the target language form readonly.

--- translator/forms.py
+++ translator/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 class GameForm(forms.Form):
-    game = forms.CharField(widget=forms.Textarea(attrs={"rows": 20, "cols": 75, 'placeholder': 'Paste PGN'}), label= '', required=False)
+    game = forms.CharField(widget=forms.Textarea(attrs={"rows": 15, "cols": 75, 'placeholder': 'Paste PGN'}), label= '', required=False)
 class TranslatedGame(forms.Form):
-      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 20, "cols": 75, 'placeholder': 'foo'}), label= '',required=False)
+      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 15, "cols": 75, 'placeholder': 'foo','readonly':'readonly'}), label= '', required=False)
