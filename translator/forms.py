2da5b508ea579fc432104f527f3c5fc5ee3223f7
Eduardo Louhau <louhaudev@gmail.com>
2023-08-06 15:43:45 -0400

    Create a dropdown menu for the languages.

--- translator/forms.py
+++ translator/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+
+LANGUAGE_CHOICES = [
+    ("en", "English"),
+    ("es", "Spanish"),
+]
+
 class GameForm(forms.Form):
+    lang_choices = forms.CharField(label=False, widget=forms.Select(choices=LANGUAGE_CHOICES), required=False)
     game = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'placeholder': 'Paste PGN','class': 'form-control'}), label= '', required=False)
 class TranslatedGame(forms.Form):
-      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'readonly':'readonly','class': 'form-control'}), label= '', required=False)
+      lang_choices = forms.CharField(label=False, widget=forms.Select(choices=LANGUAGE_CHOICES), required=False)
+      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'readonly':'readonly','class': 'form-control'}), label=False, required=False)
+      
