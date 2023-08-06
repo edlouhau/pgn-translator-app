fee1f8b8a7365f068ba3873c9e1d5fe935c1117f
Eduardo Louhau <louhaudev@gmail.com>
2023-08-06 17:22:40 -0400

    Style dropdowns menu.

--- translator/forms.py
+++ translator/forms.py
@@ -6,9 +6,9 @@ LANGUAGE_CHOICES = [
 ]
 
 class GameForm(forms.Form):
-    lang_choices = forms.CharField(label=False, widget=forms.Select(choices=LANGUAGE_CHOICES), required=False)
-    game = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'placeholder': 'Paste PGN','class': 'form-control'}), label= '', required=False)
+    lang_choices = forms.CharField(label=False, widget=forms.Select(choices=LANGUAGE_CHOICES,attrs={'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
+    game = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'placeholder': 'Paste PGN','class': 'form-control shadow'}), label= '', required=False)
 class TranslatedGame(forms.Form):
-      lang_choices = forms.CharField(label=False, widget=forms.Select(choices=LANGUAGE_CHOICES), required=False)
-      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'readonly':'readonly','class': 'form-control'}), label=False, required=False)
+      lang_choices = forms.CharField(label=False, widget=forms.Select(choices=LANGUAGE_CHOICES,attrs={'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
+      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'readonly':'readonly','class': 'form-control shadow'}), label=False, required=False)
       
