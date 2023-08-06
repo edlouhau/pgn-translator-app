6e799fa6c7f6a8438f7296c8866ab5a257056490
Eduardo Louhau <louhaudev@gmail.com>
2023-08-06 12:54:26 -0400

    Apply Bootstrap styling to the forms.

--- translator/forms.py
+++ translator/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 class GameForm(forms.Form):
-    game = forms.CharField(widget=forms.Textarea(attrs={"rows": 15, "cols": 75, 'placeholder': 'Paste PGN'}), label= '', required=False)
+    game = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'placeholder': 'Paste PGN','class': 'form-control'}), label= '', required=False)
 class TranslatedGame(forms.Form):
-      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 15, "cols": 75, 'placeholder': 'foo','readonly':'readonly'}), label= '', required=False)
+      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'readonly':'readonly','class': 'form-control'}), label= '', required=False)
