e067c14e70aa5ae6b90a77a8f3d95232c1cdedae
Eduardo Louhau <louhaudev@gmail.com>
2023-08-07 21:38:38 -0400

    Make the forms take the languages selection.

--- translator/forms.py
+++ translator/forms.py
@@ -10,20 +10,21 @@ LANGUAGE_CHOICES = [
     ("fi", "Finnish"),    # Finnish
     ("fr", "French"),     # French
     ("de", "German"),     # German
-    ("he", "Hungarian"),  # Hungarian
+    ("hu", "Hungarian"),  # Hungarian
     ("is", "Icelandic"),  # Icelandic
     ("it", "Italian"),    # Italian
     ("no", "Norwegian"),  # Norwegian
     ("pl", "Polish"),     # Polish
     ("pt", "Portuguese"), # Portuguese
     ("ro", "Romanian"),   # Romanian
-    ("sv", "Swedish"),  # Swedish
+    ("sv", "Swedish"),    # Swedish
 ]
 
 class GameForm(forms.Form):
-    lang_choices = forms.CharField(label=False, widget=forms.Select(choices=LANGUAGE_CHOICES,attrs={'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
+    source_lang_choices = forms.CharField(label='', widget=forms.Select(choices=LANGUAGE_CHOICES,attrs={'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
     game = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'placeholder': 'Paste PGN','class': 'form-control shadow'}), label= '', required=False)
+
 class TranslatedGame(forms.Form):
-      lang_choices = forms.CharField(label=False, widget=forms.Select(choices=LANGUAGE_CHOICES,attrs={'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
-      translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'readonly':'readonly','class': 'form-control shadow'}), label=False, required=False)
+    target_lang_choices = forms.CharField(label='', widget=forms.Select(choices=LANGUAGE_CHOICES,attrs={'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
+    translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'readonly':'readonly','class': 'form-control shadow'}), label='', required=False)
       
--- translator/views.py
+++ translator/views.py
@@ -12,18 +12,23 @@ def index(request):
         source_lang_form = GameForm(request.POST)
         target_lang_form = TranslatedGame(request.POST)
         
-        if source_lang_form.is_valid():
+        if source_lang_form.is_valid() and target_lang_form.is_valid():
             game = source_lang_form.cleaned_data['game']
-            source_language = "en" # TODO get from game form.
-            target_language = "es" # TODO get from game form.
+            source_language = source_lang_form.cleaned_data['source_lang_choices']
+            target_language = target_lang_form.cleaned_data['target_lang_choices']
             translated_game = translate_pgn_game(source_language, target_language, game)
-            target_lang_form = TranslatedGame(initial={'translated_pgn': translated_game})
+            target_lang_form = TranslatedGame(initial={'target_lang_choices': target_language, 'translated_pgn': translated_game})
             context['translated_game'] = translated_game
+            print('The request method is:', request.method) #TODO Remove print statements.
+            print('The POST data is:', request.POST)
+            print('Source_language', source_language)
+            print('Target_language', target_language)
+            
     else:
         source_lang_form = GameForm()
         target_lang_form = TranslatedGame()
     
-    context.update( {'source_form': source_lang_form, 'target_form': target_lang_form})
+    context.update({'source_form': source_lang_form, 'target_form': target_lang_form})
     return render(request, 'translator/index.html', context)
 
 
