1a7aab689d95f618f2a4d1cbc549304ea71ad5da
Eduardo Louhau <louhaudev@gmail.com>
2023-08-09 15:09:45 -0400

    Translate game comments.

--- translator/views.py
+++ translator/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from .pgn_translator import translate_pgn_game
 from .forms import GameForm
 from .forms import TranslatedGame
+from deep_translator import GoogleTranslator
+
+
+            
+
 
 # Create your views here.
 
 def index(request):
     context = {}
-    
     if request.method == "POST":
         source_lang_form = GameForm(request.POST)
         target_lang_form = TranslatedGame(request.POST)
@@ -17,6 +21,10 @@ def index(request):
             source_language = source_lang_form.cleaned_data['source_lang_choices']
             target_language = target_lang_form.cleaned_data['target_lang_choices']
             translated_game = translate_pgn_game(source_language, target_language, game)
+
+            # Translate game comments.
+            translated_game  = GoogleTranslator(source=source_language,target=target_language).translate(game)
+
             target_lang_form = TranslatedGame(initial={'target_lang_choices': target_language, 'translated_pgn': translated_game})
             context['translated_game'] = translated_game
            
