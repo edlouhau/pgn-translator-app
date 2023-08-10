ddeb86a04c657d7a46e60e8c7d161b6c3688ffc7
Eduardo Louhau <louhaudev@gmail.com>
2023-08-10 13:46:08 -0400

    Auto submit selections from dropdown menus.

--- translator/static/translator/index.js
+++ translator/static/translator/index.js
@@ -1,11 +1,22 @@
-//Auto submit the source language form.
 const textarea = document.getElementById('id_game');
-const form = document.getElementById('sourceForm');
+const form = document.getElementById('forms');
+const sourceDropdown = document.getElementById('id_source_lang_choices');
+const targetDropdown = document.getElementById('id_target_lang_choices');
 let delayTimer;
 
+//Auto submit the source language form.
 textarea.addEventListener('input', function () {
     clearTimeout(delayTimer);
     delayTimer = setTimeout(function () {
         form.submit();
     }, 500); // Set a delay of 1000 milliseconds (1 second)
+});
+
+//Auto submit selections from dropdown menus.
+sourceDropdown.addEventListener('change', function() {
+    form.submit();
+});
+
+targetDropdown.addEventListener('change', function() {
+    form.submit();
 });
