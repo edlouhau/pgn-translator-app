ceef1238536927d9dc5f13f9abef4377549a1abf
Eduardo Louhau <louhaudev@gmail.com>
2024-01-13 17:29:42 -0500

    Create an upload form.

--- app/pgn_translator/forms.py
+++ app/pgn_translator/forms.py
@@ -20,14 +20,26 @@ LANGUAGE_CHOICES = [
     ("sv", "Swedish"),    # Swedish
 ]
 
+GAMES_LIST = [
+    ("Game 1", "Game 1"),
+    ("Game 2", "Game 2"),
+    ("Game 3", "Game 3"),
+]
+
+class UploadPgnForm(forms.Form):
+    upload_pgn_file = forms.FileField()
+
 class GameForm(forms.Form):
-    source_lang_choices = forms.CharField(label='', widget=forms.Select(choices=LANGUAGE_CHOICES,attrs={'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
-    game = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'placeholder': 'Paste PGN','class': 'form-control shadow'}), label= '', required=False)
+    source_lang_choices = forms.CharField(label='', widget=forms.Select(choices=LANGUAGE_CHOICES, attrs={
+                                          'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
+    game = forms.CharField(widget=forms.Textarea(attrs={
+                           "rows": 10, "cols": 75, 'placeholder': 'Paste PGN', 'class': 'form-control shadow'}), label='', required=False)
 
 # Swap languages at index 0 and 1.
 LANGUAGE_CHOICES[0], LANGUAGE_CHOICES[1] = LANGUAGE_CHOICES[1], LANGUAGE_CHOICES[0]
 
 class TranslatedGame(forms.Form):
-    target_lang_choices = forms.CharField(label='', widget=forms.Select(choices=LANGUAGE_CHOICES,attrs={'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
-    translated_pgn = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 75, 'readonly':'readonly','class': 'form-control shadow'}), label='', required=False)
-      
+    target_lang_choices = forms.CharField(label='', widget=forms.Select(choices=LANGUAGE_CHOICES, attrs={
+                                          'class': 'mb-4 shadow-sm dropdown btn btn-outline-secondary'}), required=False)
+    translated_pgn = forms.CharField(widget=forms.Textarea(
+        attrs={"rows": 10, "cols": 75, 'readonly': 'readonly', 'class': 'form-control shadow'}), label='', required=False)
--- app/pgn_translator/views.py
+++ app/pgn_translator/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from app.pgn_translator.translate_pgn import translate_pgn_game, translate_tags_comments
 from .forms import GameForm
 from .forms import TranslatedGame
+from .forms import UploadPgnForm
 
+def handle_uploaded_file(f):   
+    with open('pgn-translator/uploads/'+f.name, 'wb+') as destination:   
+        for game in f.games(): 
+            destination.write(game) 
 
 def index(request):
     context = {}
+
     if request.method == "POST":
+        
+        input_pgn_file_form = UploadPgnForm(request.POST,request.FILES)
+        if input_pgn_file_form.is_valid(): 
+            handle_uploaded_file(request.FILES["upload_pgn_file"]) 
+        
         source_lang_form = GameForm(request.POST)
         target_lang_form = TranslatedGame(request.POST)
-
+        
         if source_lang_form.is_valid() and target_lang_form.is_valid():
             game = source_lang_form.cleaned_data['game']
             source_language = source_lang_form.cleaned_data['source_lang_choices']
@@ -28,10 +39,12 @@ def index(request):
     else:
         source_lang_form = GameForm()
         target_lang_form = TranslatedGame()
+        input_pgn_file_form = UploadPgnForm()
 
     context.update({'source_form': source_lang_form,
-                   'target_form': target_lang_form})
+                   'target_form': target_lang_form,'input_pgn_file': input_pgn_file_form })
     return render(request, 'pgn_translator/index.html', context)
 
 def about(request):
     return render(request, "pgn_translator/about.html")
+
